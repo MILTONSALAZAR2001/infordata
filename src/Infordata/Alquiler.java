b71c1ac8acabda6ae1de6c4e6b6116ac884dc71b
MILTONSALAZAR2001 <miltsfour67448812@gmail.com>
2021-09-03 11:56:37 -0400

    interface

---
+++ src/Infordata/Alquiler.java
@@ -0,0 +1,23 @@
+package Infordata;
+
+import interfaces.IReseteable;
+
+public class Alquiler extends  Producto  {
+    protected double precioHora;
+    public  Alquiler(String codigo, String modelo, double precioHora){
+        super(codigo, modelo);
+        this.setPrecioHora(precioHora);
+
+    }
+
+    public double getPrecioHora() {
+        return precioHora;
+    }
+
+    public void setPrecioHora(double precioHora) {
+        this.precioHora = precioHora;
+    }
+
+
+    }
+
---
+++ src/Infordata/DiscoDuro.java
@@ -0,0 +1,24 @@
+package Infordata;
+
+import interfaces.IReseteable;
+
+public class DiscoDuro extends  Alquiler implements IReseteable {
+    private int capacidad;
+    public DiscoDuro(String codigo, String modelo, double precioHora, int capacidad){
+        super(codigo, modelo, precioHora);
+
+
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+    @Override
+    public void reset() {
+        System.out.println("Reseteando hdd...");
+    }
+}
